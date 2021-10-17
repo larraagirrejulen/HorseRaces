e1c57a34853452d303a0bda0b08a7f1eb804c800
Julen <larraagirrejulen@gmail.com>
2021-10-17 11:48:14 +0200

    Mockito test cases

--- src/test/java/CreateRaceHorseMockIntTest.java
+++ src/test/java/CreateRaceHorseMockIntTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 import java.util.Date;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -16,16 +14,18 @@ import domain.Horse;
 import domain.Race;
 import domain.RaceHorse;
 import domain.StartTime;
+import exceptions.HorseDoesntExist;
+import exceptions.RaceDoesntExist;
+import exceptions.RaceFinished;
+import exceptions.RaceFullException;
+import exceptions.RaceHorseAlreadyExist;
+import exceptions.WrongParameterException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CreateRaceHorseMockIntTest {
 	
-	//Race mockedRace=Mockito.mock(Race.class);
-	
-//	@Mock
 	DataAccess horseRacesDAO = Mockito.mock(DataAccess.class);
 	
-//	@InjectMocks
 	BLFacade sut = new BLFacadeImplementation(horseRacesDAO);
 	
 	@Test
@@ -42,6 +42,148 @@ public class CreateRaceHorseMockIntTest {
 			assertEquals(rh.getRace(), race);
 			assertEquals(rh.getHorse(), horse);
 			
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test2(){
+		try {	
+			Horse horse = new Horse("Julen", "Belauntza", 20, "male", 99);
+			double winGain = 1.5;
+			
+			Mockito.doThrow(new WrongParameterException()).when(horseRacesDAO).createRaceHorse(winGain, null, horse);
+			sut.createRaceHorse(winGain, null, horse);
+			fail();
+			
+		}catch(WrongParameterException e) {
+			assertTrue(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test3(){
+		try {
+			Race race = new Race(new Date(), 4, new StartTime("10:30"));	
+			double winGain = 1.5;
+			
+			Mockito.doThrow(new WrongParameterException()).when(horseRacesDAO).createRaceHorse(winGain, race, null);
+			sut.createRaceHorse(winGain, race, null);
+			fail();
+			
+		}catch(WrongParameterException e) {
+			assertTrue(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test4(){
+		try {
+			Race race = new Race(new Date(), 4, new StartTime("10:30"));	
+			Horse horse = new Horse("Julen", "Belauntza", 20, "male", 99);
+			double winGain = 0.5;
+			
+			Mockito.doThrow(new WrongParameterException()).when(horseRacesDAO).createRaceHorse(winGain, race, horse);
+			sut.createRaceHorse(winGain, race, horse);
+			fail();
+			
+		}catch(WrongParameterException e) {
+			assertTrue(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test5(){
+		try {
+			Race race = new Race(new Date(), 4, new StartTime("10:30"));	
+			Horse horse = new Horse("Julen", "Belauntza", 20, "male", 99);
+			double winGain = 1.5;
+			
+			Mockito.doThrow(new RaceDoesntExist()).when(horseRacesDAO).createRaceHorse(winGain, race, horse);
+			sut.createRaceHorse(winGain, race, horse);
+			fail();
+			
+		}catch(RaceDoesntExist e) {
+			assertTrue(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test6(){
+		try {
+			Race race = new Race(new Date(), 4, new StartTime("10:30"));	
+			Horse horse = new Horse("Julen", "Belauntza", 20, "male", 99);
+			double winGain = 1.5;
+			
+			Mockito.doThrow(new HorseDoesntExist()).when(horseRacesDAO).createRaceHorse(winGain, race, horse);
+			sut.createRaceHorse(winGain, race, horse);
+			fail();
+			
+		}catch(HorseDoesntExist e) {
+			assertTrue(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test7(){
+		try {
+			Race race = new Race(new Date(), 4, new StartTime("10:30"));	
+			Horse horse = new Horse("Julen", "Belauntza", 20, "male", 99);
+			double winGain = 1.5;
+			
+			Mockito.doThrow(new RaceFinished()).when(horseRacesDAO).createRaceHorse(winGain, race, horse);
+			sut.createRaceHorse(winGain, race, horse);
+			fail();
+			
+		}catch(RaceFinished e) {
+			assertTrue(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test8(){
+		try {
+			Race race = new Race(new Date(), 4, new StartTime("10:30"));	
+			Horse horse = new Horse("Julen", "Belauntza", 20, "male", 99);
+			double winGain = 1.5;
+			
+			Mockito.doThrow(new RaceHorseAlreadyExist()).when(horseRacesDAO).createRaceHorse(winGain, race, horse);
+			sut.createRaceHorse(winGain, race, horse);
+			fail();
+			
+		}catch(RaceHorseAlreadyExist e) {
+			assertTrue(true);
+		}catch(Exception e){
+			e.printStackTrace();
+			fail();
+		}
+	}
+	@Test
+	public void test9(){
+		try {
+			Race race = new Race(new Date(), 4, new StartTime("10:30"));	
+			Horse horse = new Horse("Julen", "Belauntza", 20, "male", 99);
+			double winGain = 1.5;
+			
+			Mockito.doThrow(new RaceFullException()).when(horseRacesDAO).createRaceHorse(winGain, race, horse);
+			sut.createRaceHorse(winGain, race, horse);
+			fail();
+			
+		}catch(RaceFullException e) {
+			assertTrue(true);
 		}catch(Exception e){
 			e.printStackTrace();
 			fail();
