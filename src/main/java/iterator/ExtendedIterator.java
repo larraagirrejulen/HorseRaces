65d373a98a98a13a0c5b45250a763a7f2ec4cbf0
Julen <larraagirrejulen@gmail.com>
2021-11-01 05:22:09 +0100

    Iterator Patroia ExtendedIterator interfazea

---
+++ src/main/java/iterator/ExtendedIterator.java
@@ -0,0 +1,29 @@
+package iterator;
+
+import java.util.Iterator;
+
+public interface ExtendedIterator extends Iterator<Object>{
+	
+	/**
+	 * Aurreko elementuan kokatu
+	 * @return Uneko elementua
+	 */
+	public Object previous();
+	
+	/**
+	 * Aurreko elementua existitzen den ikusten du.
+	 * @return true existitzen bada, false bestela.
+	 */
+	public boolean hasPrevious();
+	
+	/**
+	 * Lehendabiziko elementuan kokatzen da.
+	 */
+	public void goFirst();
+	
+	/**
+	 * Azkeneko elementuan kokatzen da.
+	 */
+	public void goLast();
+	
+}
